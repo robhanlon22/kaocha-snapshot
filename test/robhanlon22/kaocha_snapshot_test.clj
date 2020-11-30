d0b60d0c0a75556bac72d616a141e7878c735d01
Rob Hanlon <robhanlon@gmail.com>
2020-11-30 07:34:09 +0000

    Initial commit, add generated project

---
+++ test/robhanlon22/kaocha_snapshot_test.clj
@@ -0,0 +1,7 @@
+(ns robhanlon22.kaocha-snapshot-test
+  (:require [clojure.test :refer :all]
+            [robhanlon22.kaocha-snapshot :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
