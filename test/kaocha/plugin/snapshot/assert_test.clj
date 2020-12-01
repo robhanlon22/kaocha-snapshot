e6e86540811f647e21bc6e6c5781817159e9c921
Rob Hanlon <rob.hanlon@highspot.com>
2020-12-01 01:18:19 -0800

    Add coverage

--- test/kaocha/plugin/snapshot/assert_test.clj
+++ test/kaocha/plugin/snapshot/assert_test.clj
@@ -1,6 +1,5 @@
 (ns kaocha.plugin.snapshot.assert-test
   (:require
-   [clojure.java.io :as io]
    [clojure.string :as str]
    [clojure.test :refer [deftest is]]
    [kaocha.plugin.snapshot.assert :as assert]))
@@ -14,23 +13,33 @@
 (deftest snapshot-path-test
   (is (= "foo/bar/baz/qux_1.edn" (assert/snapshot-path 'baz/q+u*x "foo/bar" 1))))
 
-(def value {:ham "eggs"})
-
-(defn ensure-snapshot!
-  [id]
-  (let [base-path (System/getProperty "java.io.tmpdir")]
-    (assert/ensure-snapshot! id base-path 1 value)))
+(defn ^:private base-path
+  []
+  (System/getProperty "java.io.tmpdir"))
 
 (deftest ensure-snapshot!-test
-  (let [out  (java.io.StringWriter.)
-        id   (gensym "ensure-snapshot")
-        path (binding [*out* out] (ensure-snapshot! id))]
-    (try
-      (is (= value (assert/read-edn! path)) "wrote the snapshot")
-      (let [message (str out)]
-        (is (str/includes? message "Wrote snapshot"))
-        (is (str/includes? message path)))
-      (let [message (with-out-str (ensure-snapshot! id))]
-        (is (empty? message)))
-      (finally
-        (io/delete-file path)))))
+  (let [out              (java.io.StringWriter.)
+        id               (gensym "ensure-snapshot!-test")
+        value            {:ham "eggs"}
+        ensure-snapshot! #(assert/ensure-snapshot! id (base-path) 1 value)
+        path             (binding [*out* out] (ensure-snapshot!))]
+    (is (= value (assert/read-edn! path)) "wrote the snapshot")
+    (let [message (str out)]
+      (is (str/includes? message "Wrote snapshot"))
+      (is (str/includes? message path)))
+    (let [message (with-out-str (ensure-snapshot!))]
+      (is (empty? message)))))
+
+(deftest with-context-test
+  (let [id (gensym "with-context-test")]
+    (assert/with-context id (base-path)
+      (is (= id assert/*id*))
+      (is (= (base-path) assert/*base-path*))
+      (is (= 0 @assert/*counter_*)))))
+
+(deftest match-snapshot?-test
+  (let [id (gensym "match-snapshot?-test")]
+    (assert/with-context id (base-path)
+      (is (match-snapshot? {:foo "bar"}))
+      (is (match-snapshot? {:banana "split"}))
+      (is (= 2 @assert/*counter_*)))))
